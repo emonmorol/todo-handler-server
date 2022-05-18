fd3c5bd9c3af4b6c6a1c58c2ac13ac5eda4c103e
emonmorol <ekramulhasane69@gmail.com>
2022-05-19 00:44:46 +0600

    postind todo api created

---
+++ index.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+const app = express();
+const port = process.env.PORT || 5000;
+
+//middleware
+app.use(cors());
+app.use(express.json());
+
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@todo-handler.shloa.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+async function run() {
+  try {
+    await client.connect();
+    const todoCollection = client.db("todoHandler").collection("todos");
+
+    app.post("/todoList", async (req, res) => {
+      const todoDoc = req.body;
+      const result = await todoCollection.insertOne(todoDoc);
+      res.send(result);
+    });
+  } finally {
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("ToDo handler is active");
+});
+
+app.listen(port, () => {
+  console.log(`to do running from port ${port}`);
+});
